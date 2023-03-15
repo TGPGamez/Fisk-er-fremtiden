951c44e80d6b96234c80161124a73013c95f0ab5
MarcusBou <60425915+MarcusBou@users.noreply.github.com>
2023-03-15 13:58:01 +0100

    Game

--- SSP - Unity/Assets/Tobias Test scene/Game.cs
+++ SSP - Unity/Assets/Tobias Test scene/Game.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    [SerializeField] private float foodIndicator; 
-    [SerializeField] private float waterIndicator; 
-    [SerializeField] private float medicinIndicator; 
+    [SerializeField] private float fish; 
+    [SerializeField] private float fishWeight; 
+    [SerializeField] private float food; 
+    [SerializeField] private float waterPollution; 
+    [SerializeField] private float sickness; 
 
     public void StartScene()
     {
@@ -21,5 +23,97 @@ public class Game : MonoBehaviour
     public void EndScene()
     {
 
+    }
+
+    public void FishEat()
+    {
+        food -= 1 ;
+        if( food > 50 && food <= 70)
+        {
+            fishWeight += 50;
+            waterPollution += 2;
+        }
+        else if ( food > 70)
+        {
+            fishWeight += 50;
+            waterPollution += 4;
+        }
+        else if (food > 25 && food <= 50)
+        {
+            fishWeight += 20;
+            waterPollution += 1;
+        }
+        else
+        {
+            float survivalRate = food / 25 * 100;
+            int fishSurvival = Random.Range(0, 101);
+            if (fishSurvival > survivalRate)
+            {
+                fish -= 1;
+                waterPollution += 15;
+            }
+        }
+
+        if(waterPollution < 20) { }
+        else if (waterPollution >= 20 && waterPollution < 40) { sickness += 1; }
+        else if (waterPollution >= 40 && waterPollution < 60) { sickness += 2; }
+        else if (waterPollution >= 60 && waterPollution < 80) { sickness += 4; }
+        else if (waterPollution >= 80) { sickness += 8; }
+
+        if (sickness > 15)
+        {
+            float deathrate = sickness / 85 * 100;
+            int fishSurvival = Random.Range(1, 100);
+            if (fishSurvival < deathrate)
+            {
+                fish -= 1;
+                waterPollution += 15;
+            }
+        }
+
+        if(waterPollution > 100) { waterPollution = 100; }
+        if(sickness > 100) { sickness = 100; }
+        if(food < 0) { food = 0; }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            FeedFish(); 
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.CompareTag("Medicin"))
+        {
+            MedicinateFish();
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void FeedFish()
+    {
+        food += 25;
+        if (food > 100)
+        {
+            food = 100;
+        }
+    }
+
+    private void MedicinateFish()
+    {
+        sickness -= 35;
+        if (sickness < 0)
+        {
+            sickness = 0;
+        }
+    }
+
+    private void cleanWater()
+    {
+        waterPollution -= 50;
+        if(waterPollution < 0)
+        {
+            waterPollution = 0;
+        }
     }
 }
