30e0650c9bf70b32e2152a2cb560f1273528fa6d
TGPGamez <grang1221@gmail.com>
2023-03-16 12:03:44 +0100

    Api call

---
+++ SSP - Unity/Assets/Tobias Test scene 2/UserPoints.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserPoints
+{
+    public UserPoints(string name, int highestWeight, int mostFish, int points)
+    {
+        Name = name;
+        HighestWeight = highestWeight;
+        MostFish = mostFish;
+        Points = points;
+    }
+
+    public string Name { get; set; }
+    public int HighestWeight { get; set; }
+    public int MostFish { get; set; }
+    public int Points { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Weight: {HighestWeight}, Fish amount: {MostFish}, Points: {Points}";
+    }
+}
