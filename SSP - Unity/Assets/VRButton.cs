ee777aa3a04fc35ea0d1ac8beb7ccd48f2db68b3
Marcus Bournonwille <marc8847@zbc.dk>
2023-03-22 12:39:03 +0100

    Update button and lever

---
+++ SSP - Unity/Assets/VRButton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem.Composites;
+
+public class VRButton : MonoBehaviour
+{
+    public GameObject Button;
+    public UnityEvent OnPress;
+    public UnityEvent OnRelease;
+    private GameObject presser;
+    private bool ispressed;
+    private Vector3 originalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = Button.transform.localPosition;
+        ispressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!ispressed)
+        {
+            Vector3 temp = originalPosition;
+            temp.z = temp.z + 0.05f;
+            Button.transform.localPosition = temp;
+            presser = other.gameObject;
+            OnPress.Invoke();
+            ispressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("FUCK");
+        if (other.gameObject == presser)
+        {
+            Button.transform.localPosition = originalPosition;
+            OnRelease.Invoke();
+            ispressed = false;
+        }
+    }
+
+}
